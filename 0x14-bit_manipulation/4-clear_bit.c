2bfcce46c930d26ff2ce5a3cc18fe3c22832aa17
1409e <ellawetindi@gmail.com>
2023-04-20 05:57:08 -0700

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * clear_bit - Sets the value of a bit at a given index to 0.
@@ -7,13 +9,18 @@
  *
  * Return: If an error occurs - -1.
  *         Otherwise - 1.
- */
+*/
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
-		return (-1);
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		char *error = malloc(100 * sizeof(char));
 
-	*n &= ~(1 << index);
-
+		sprintf(error, "Error: invalid index %u", index);
+		fprintf(stderr, "%s\n", error);
+		free(error);
+		exit(1);
+	}
+	*n &= ~(1UL << index);
 	return (1);
 }
