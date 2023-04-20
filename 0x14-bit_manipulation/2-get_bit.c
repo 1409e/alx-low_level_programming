79061268179475504e5a0c6e618b9931645ed815
1409e <ellawetindi@gmail.com>
2023-04-20 05:39:57 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,14 +7,11 @@
  *
  * Return: If an error occurs - -1.
  *         Otherwise - The value of bit at index.
- */
+*/
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
-		return (-1);
+	if (index >= 8 * sizeof(unsigned long int))
+	return (-1);
 
-	if ((n & (1 << index)) == 0)
-		return (0);
-
-	return (1);
+	return ((n >> index) & 1);
 }
